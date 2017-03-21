0fc50c1096e14ea4b64da206fefa597117361d6d
Vova <vovakulikov@icloud.com>
2017-03-21 15:55:32 +0300

    Добавляет конфигурацию сервера

---
+++ server/config.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Vova on 21.03.2017.
+ */
+
+const config = {
+  _distPath: './server/storeFiles/',
+  firebaseConfig : {
+    apiKey: "AIzaSyArx5L61a2Jkho2kxrYNpLCjS2L1FAiOr8",
+    authDomain: "excel-list.firebaseapp.com",
+    databaseURL: "https://excel-list.firebaseio.com",
+    storageBucket: "excel-list.appspot.com",
+    messagingSenderId: "404882626777"
+  }
+}
+
+exports.config = config;
