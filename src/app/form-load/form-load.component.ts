5e6ca5152bfef3955c9eea7c2d2817d4856aabbc
Vova <vovakulikov@icloud.com>
2017-03-23 11:40:41 +0300

    Обновляет компонент формы загрузки файлов

--- src/app/form-load/form-load.component.ts
+++ src/app/form-load/form-load.component.ts
@@ -2,31 +2,40 @@ import { Component, OnInit } from '@angular/core';
 
 import { HelperService } from '../shared/helper.service';
 
+import { StoreService } from '../shared/store.service';
+
+
 @Component({
   selector: 'app-form-load',
   templateUrl: './form-load.component.html',
   styleUrls: ['./form-load.component.css']
 })
 export class FormLoadComponent implements OnInit {
-  correctFiles:[File];
-  uncorrectFiles:[File];
+  correctFiles: File[];
+  uncorrectFiles: File[];
 
-  constructor(private helpService: HelperService){}
-
+  constructor(private helpService: HelperService,private storeService: StoreService ){
+    this.correctFiles = [];
+    this.uncorrectFiles = [];
+  }
   ngOnInit() {}
 
   onSubmit(evt){
-    this.helpService._Submit(this.correctFiles);
-    //console.log('Нажали на кнопку отправки и вот наше событие ', evt);
+    let files = this.storeService.getFiles();
+    this.helpService._Submit(files)
+      .then((files)=>{
+          console.log('Данные пришедшие после отравки данных на сервер',files)
+          this.storeService.addServerFile(files);
+      })
+    this.storeService.removeAll();
   }
-  onChange(fileInput:any){
-    let r = new Promise((resolve,reject)=>{});
-    console.log(r);
-
-    [this.correctFiles,this.uncorrectFiles] = this.helpService.checkGroupFiles(fileInput);
-    console.log(this.correctFiles,this.uncorrectFiles)
-    //this.helpService.checkGroupFiles(fileInput)
-    //this.helpService.logg(fileInput)
-    //console.log('changing form',fileInput)
+  onChange(fileInput){
+    this.storeService.removeAll();
+    [this.correctFiles,this.uncorrectFiles] = this.storeService.checkGroupFiles(fileInput);
+    this.storeService.addData(this.correctFiles);
+    console.log('Тащим из сервиса только что пришедшие даннцые',this.storeService.getFiles())
+  }
+  onClickGetData(){
+    this.helpService.getData();
   }
 }
