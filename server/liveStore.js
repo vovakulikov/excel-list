9fa3abdc1a5d052ca8b5ef167af0e51aef93f610
Vova <vovakulikov@icloud.com>
2017-04-11 14:08:36 +0300

    Update liveStore

--- server/liveStore.js
+++ server/liveStore.js
@@ -1,7 +1,7 @@
-const clients ={};
+const clients = {};
 
 exports.subcribe = function (req, res){
-  clients[req.user.email] =  clients[req.user.email] || [];
+  clients[req.user.email] = clients[req.user.email] || [];
   clients[req.user.email].push(res);
   res.on('close',function(){
     clients[req.user.email].splice(clients[req.user.email].indexOf(res), 1);
@@ -9,6 +9,7 @@ exports.subcribe = function (req, res){
 };
 
 exports.publish = function (req, documentsInfo) {
+  console.log(clients[req.user.email].length)
   clients[req.user.email].forEach((res) => {
     res.json(documentsInfo);
   });
